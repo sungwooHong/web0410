e30fcac667519dc1797af8e8d4e60c7159a036a8
sungwooHong <37567198+sungwooHong@users.noreply.github.com>
2018-04-10 14:43:53 +0900

    체크박스 반복

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 var express = require('express');
+var _= require('underscore');
 var app = express();
 
-// var bodyParser = require('body-parser');
-// app.use(bodyParser.json()); // support json encoded bodies
-// app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+ var bodyParser = require('body-parser');
+ app.use(bodyParser.json()); // support json encoded bodies
+ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get('/', (req, res) => {
   res.charset = 'UTF-8';
+  console.log(req.query);
   res.send('GET으로 넘어온 name은 '+ req.query.name + '입니다.');
 } );
 
-/*
+//template 템플릿
+app.get('/rowcol', (req, res) =>{
+res.charset='UTF-8';
+  var checkbox = '<input type= "checkbox">';
+  var row = req.query.row;
+  var col = req.query.col;
+  var string = '';
+  string +='<form>';
+  for (var i = 0; i< col; i++){
+  string += checkbox;
+  }
+  string +='</form>';
+
+  var result = '';
+  for(var i=0; i < row; i++) {
+    result += string;
+  }
+
+  res.send(result);
+} );
+
 app.post('/', (req, res) => {
   res.charset = 'UTF-8';
   res.send('POST로 넘어온 name은 '+ req.body.name + '입니다.');
 } );
-*/
+
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
